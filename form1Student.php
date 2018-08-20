404f4e7b140d8eaa2254cef3f1f70beba407f75f
Chethiya Gunawardana <chethiya.gunawardana1996@gmail.com>
2018-08-20 22:21:16 +0530

    form validations

--- form1Student.php
+++ form1Student.php
@@ -37,51 +37,54 @@
 							<h4>Internship Acceptance Form</h4>
                         </div>
                     </div>
-					<form>
+					
+					<!--Form filled by student-->	
+					<form name='form1student' method='POST' action=' '>
 						<fieldset>
-							<div class="form-group">
-							  <label><b>Student ID</b></label>
-							  <input class="form-control" id="stdID" placeholder="Enter Student ID" type="text">
+							<div class='form-group'>
+								<label><b>Student ID</b></label>
+								<input class='form-control' name='stdID' placeholder='Enter Student ID' type='text'>
 							</div>
-							<div class="form-group">
-							  <label><b>Address</b></label>
-							  <input class="form-control" id="address" placeholder="Enter Address" type="text">
+							<div class='form-group'>
+								<label><b>Address</b></label>
+							    <input class='form-control' name='address' placeholder='Enter Address' type='text'>
+							</div>	
+							<div class='form-group'>
+								<label><b>Home Phone</b></label>
+								<input class='form-control' name='hphone' placeholder='Enter Home Phone' type='text'>
 							</div>
-							<div class="form-group">
-							  <label><b>Home Phone</b></label>
-							  <input class="form-control" id="hphone" placeholder="Enter Home Phone" type="text">
+							<div class='form-group'>
+								<label><b>Mobile Phone</b></label>
+								<input class='form-control' name='mphone' placeholder='Enter Mobile Phone' type='text'>
 							</div>
-							<div class="form-group">
-							  <label><b>Mobile Phone</b></label>
-							  <input class="form-control" id="mphone" placeholder="Enter Mobile Phone" type="text">
+							<div class='form-group'>
+								<label><b>Email address</b></label>
+								<input class='form-control' name='email1' placeholder='Enter Email' type='email'>
+								<small class='form-text text-muted'><b>Use commas(,) to separate multiple email addreses.</b></small>
 							</div>
-							<div class="form-group">
-							  <label><b>Email address</b></label>
-							  <input class="form-control" id="email1" placeholder="Enter Email" type="email">
-							  <small class="form-text text-muted"><b>Use commas(,) to separate multiple email addreses.</b></small>
+							<div class='form-group'>
+								<label for='exampleSelect1'><b>Year</b></label>
+								<select class='form-control' name='year'>
+									<option>2</option>
+									<option>3</option>
+									<option>4</option>
+								</select>
 							</div>
-							<div class="form-group">
-							  <label for="exampleSelect1"><b>Year</b></label>
-							  <select class="form-control" id="year">
-								<option>2</option>
-								<option>3</option>
-								<option>4</option>
-							  </select>
+							<div class='form-group'>
+								<label for='exampleSelect1'><b>Semester</b></label>
+								<select class='form-control' name='sem'>
+									<option>1</option>
+									<option>2</option>
+								</select>
 							</div>
-							<div class="form-group">
-							  <label for="exampleSelect1"><b>Semester</b></label>
-							  <select class="form-control" id="sem">
-								<option>1</option>
-								<option>2</option>
-							  </select>
+							<div class='form-group'>
+								<label><b>CGPA</b></label>
+								<input class='form-control' name='cgpa' placeholder='Enter CGPA' type='text'>
 							</div>
-							<div class="form-group">
-							  <label><b>CGPA</b></label>
-							  <input class="form-control" id="cgpa" placeholder="Enter CGPA" type="text">
-							</div>
-							<button type="button" class="btn btn-primary">Submit</button>
+							<button type='submit' class='btn btn-primary' name='submitStudent'>Submit</button>
 						</fieldset>
 					</form>
+					
         </div>
 		
 		
@@ -90,6 +93,56 @@
     </div>
 </div>
 </section>
+
+
+<?php
+if($_SERVER['REQUEST_METHOD']=='POST')
+{
+	if(isset($_POST['submitStudent']))
+	{
+		$sid=$_POST['stdID'];
+		$address=$_POST['address'];
+		$hphn=$_POST['hphone'];
+		$mphn=$_POST['mphone'];
+		$email=$_POST['email1'];
+		$year=$_POST['year'];
+		$sem=$_POST['sem'];
+		$cgpa=$_POST['cgpa'];
+		
+		/**
+			Form Validation
+		**/
+		if(empty($sid)||empty($address)||empty($hphn)||empty($mphn)||empty($email)||empty($cgpa))
+		{
+			echo"<script>alert('One are more fields are empty')</script>";
+		}
+		else if(!preg_match("/^IT|BM|EN[0-9]{8}$/",$sid)||!preg_match("/^[0-9]{10}$/",$hphn)||!preg_match("/^[0-9]{10}$/",$mphn)||!preg_match("/^[0-3]{1}.[0-99]$/",$cgpa)||!preg_match("/^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,})$/i",$email))
+		{
+			if(!preg_match("/^IT|BM|EN[0-9]{8}$/",$sid))
+			{
+				echo"<script>alert('Invalid Student ID')</script>";		
+			}
+			if(!preg_match("/^[0-9]{10}$/",$hphn))
+			{
+				echo"<script>alert('Invalid Home Phone Number')</script>";	
+			}
+			if(!preg_match("/^[0-9]{10}$/",$mphn))
+			{
+				echo"<script>alert('Invalid Mobile Phone Number')</script>";	
+			}
+			if(!preg_match("/^[0-3]{1}.[0-99]$/",$cgpa))
+			{
+				echo"<script>alert('Invalid CGPA')</script>";	
+			}
+			if(!preg_match("/^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,})$/i",$email))
+			{
+				echo"<script>alert('Invalid Email Address')</script>";	
+			}
+		}
+	}
+}
+?>
+
 
 <!--Include Footer from another file-->
 <?php include('inc/footer.php')?>
