d1ee5e31b1cc9123f770aaf12854e7aacdf4d069
nethmi2 <nethmi2nethu@gmail.com>
2018-09-06 14:19:09 +0530

    1015 : Designing the content of the pdf

--- schedule_report.php
+++ schedule_report.php
@@ -6,11 +6,7 @@
 </style>
 
 <!--Include DB connection from another file-->
-<?php
-    include('DBConnection.php');
-    $query="select Reg_no,Name,Company,Start_date,End_date,Viva_date,Time from schedule_tab";
-    $result=mysqli_query($con,$query);
-?>
+
 
 
 <!--Include Navbar from another file-->
@@ -37,8 +33,9 @@
                 </div>
         </div>
         <!--End Sidebar Section-->
-
+        <script src="js/print button.js"></script>
         <!--Start Main section-->
+        <body class="as">
         <div class="col col-md-9 col-lg-9">
                     <!--<div class="jumbotron jumbotron-fluid text-center welcome">
                         <div class="container">
@@ -47,7 +44,65 @@
                     </div>-->
         
         <!--End main section-->
+        <div class="card">
+    <h3 class="card-header text-center font-weight-bold text-uppercase py-4">Viva Schedule Report</h3>
+    <div class="card-body">
+    <div id="table" class="table-editable">
+            <!--<span class="table-add float-right mb-3 mr-2"><a href="#!" class="text-success"><i class="fa fa-plus fa-2x" aria-hidden="true"></i></a></span>-->
+
+            
+			
+            <table class="table table-bordered table-responsive-md table-striped text-center" >
+                <tr>
+                    <th class="text-center">Registration No</th>
+                    <th class="text-center">Name</th>
+                    <th class="text-center">Company Name</th>
+                    <th class="text-center">Start Date</th>
+                    <th class="text-center">End Date</th>
+                    <th class="text-center">Viva Date </th> 
+                    <th class="text-center">Time </th>    
+                </tr>
+                
+                <?php
+                
+                
+                    while($rows=mysqli_fetch_assoc($result))
+                    {      
+                 ?>
+                <tr>
+
+                <td class="pt-3-half" contenteditable="true" > </td>
+                <td class="pt-3-half" contenteditable="true"></td>
+                <td class="pt-3-half" contenteditable="true"></td>
+                <td class="pt-3-half" contenteditable="true"></td>
+                <td class="pt-3-half" contenteditable="true"></td>
+                <td class="pt-3-half" contenteditable="true"></td>
+                <td class="pt-3-half" contenteditable="true"> </td>
+                
+                </tr>
+
+
+                <?php
+                
+                }
+                ?>
+                     
+        </table>
+
         
+        </div>
+    </div>
+</div>
+<br>
+<div>
+
+             <button class="btn btn-primary hidden-print" style="float:right;" onclick="myFunction()"><span class="glyphicon glyphicon-print" aria-hidden="false"></span> Print</button>
+      
+</div>
+</body>
+
+
+
         
 
 
