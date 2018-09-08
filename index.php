69c3ccb6e7a834973d2f6fd9fe1781904710ae91
KTIkelaniyage <thilina.kelaniyage@my.sliit.lk>
2018-09-08 23:37:03 +0530

    updated

--- index.php
+++ index.php
@@ -1,5 +1,11 @@
 <!--Include header from another file-->
 <?php include('inc/header.php'); ?>
+<?php
+$login = Session::get('userLogin');
+if($login==false){
+    header("Location:login.php");
+}
+?>
 
 <style>
 
@@ -16,16 +22,28 @@
         <div class="col col-md-3 col-lg-3 text-center">
                 <div class="card">
                     <div class="card-body">
-                        <img src="img/mlogo.png" alt="" class="img-fluid rounded-circle w-50 mb-1">
-                        <h4>Rajitha lakshan</h4>
-                        <h5 class="text-muted">Student</h5>
+                        <img src="<?php echo Session::get('photo');?>" alt="" class="img-fluid rounded-circle w-50 mb-1">
+                        <h4><?php echo Session::get('name');?></h4>
+                        <h5 class="text-muted"><?php echo Session::get('role');?></h5>
                         <div class="list-group">
                             <a href="index.php" class="list-group-item list-group-item-action active">Home</a>
-                            <a href="" class="list-group-item list-group-item-action">Functions</a>
+                            <a href="register_supervisor.php" class="list-group-item list-group-item-action" style="<?php if(Session::get('role')!="CMP"){echo "display:none";}?>">Register Supervisor</a>
+                            <a href="student_list.php" class="list-group-item list-group-item-action" style="<?php if(Session::get('role')!="CMP"){echo "display:none";}?>">Allocate Supervisor</a>
+							<a href="form1Student.php" class="list-group-item list-group-item-action" style="<?php if(Session::get('role')!="STD"){echo "display:none";}?>">Form I-1</a>
+							<a href="form1SupervisorRList.php" class="list-group-item list-group-item-action" style="<?php if(Session::get('role')!="SUP"){echo "display:none";}?>">Form I-1
+							<?php
+								include('DBConnection.php');
+								$supId=Session::get('uid');
+								$sql="SELECT * FROM form1_student_details WHERE supervisor='$supId' AND sup_response='in progress'";
+								$result=mysqli_query($con,$sql);
+								$count=mysqli_num_rows($result);
+								echo "&nbsp&nbsp&nbsp<b>$count</b>";
+							?>	
+							</a>	
+                            <a href="form-i-3.php" class="list-group-item list-group-item-action">Form I-3</a>
+                            <a href="form-i-3-supervisor.php" class="list-group-item list-group-item-action">Certify And Email Form I-3</a>
                             <a href="grade.php" class="list-group-item list-group-item-action">Grading-From</a>
                             <a href="marking_summary.php" class="list-group-item list-group-item-action">Marking-Summary-From</a>
-						    <a href="form1Student.php" class="list-group-item list-group-item-action">Form I-1</a>
-
                         </div>
                     </div>
                 </div>
