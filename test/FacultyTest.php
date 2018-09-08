111667151c6f03ff4f5fb6f0176057483106d7a0
Nalin Pushpakumara <nalinone77@gmail.com>
2018-09-09 00:56:06 +0530

    Revert "rename classes in facultyTest.php"
    This reverts commit 0b4091b3564c724b2b1d2c0ca2a56da69028c69b.

--- test/FacultyTest.php
+++ test/FacultyTest.php
@@ -7,7 +7,7 @@ class FacultyTest extends PHPUnit_Framework_TestCase
 {
 /*Test whether faulty is insert correctly and get specific faculty correctly*/
     /**
-     * @covers Faculty::addFaculty
+     * @covers BankAccount::getBalance
      */
     public function testGetOneFaculty()
     {
