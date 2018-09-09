94f3dcbbde95c99dcfdd0edcb444763212b37ce3
Nalin Pushpakumara <nalinone77@gmail.com>
2018-09-09 07:37:38 +0530

    Update PerformanceTest.php

--- test/PerformanceTest.php
+++ test/PerformanceTest.php
@@ -3,7 +3,7 @@ $filepath = realpath(dirname(__FILE__));
 include_once ($filepath.'/../classes/Examiner.php');
 use PHPUnit\Framework\TestCase;
 
-class FacultyTest extends PHPUnit_Framework_TestCase
+class PerformanceTest extends PHPUnit_Framework_TestCase
 {
 /*Test whether faulty is insert correctly and get specific faculty correctly*/
     public function testAddPerformance()
