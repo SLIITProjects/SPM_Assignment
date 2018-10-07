d09fbeb3ad7285d0aa10059fd0e7bcacfc6050ad
ranmal123 <ranmalmendis9@gmail.com>
2018-10-07 08:25:15 +0530

    Email

--- email.php
+++ email.php
@@ -47,6 +47,27 @@ include('DBConnection-I-3.php');
 						} else {
 							echo "0 results";
 						}
+						$sql6="SELECT * FROM i3_details  WHERE studentId='$student'";
+						$result6=mysqli_query($con,$sql6);
+						if ($result6->num_rows > 0) {
+							// output data of each row
+							while($row = $result6->fetch_assoc()) {
+								// $StudentId= $row["studentId"];
+								// $address= $row["address"];
+								// $contact= $row["contact"];
+								// $email= $row["email"];
+								//$address= $row["studentId"];
+								$problem=$row["problems"];
+                                $summary=$row["summary"];
+                                $month=$row["month"];
+                               
+
+							}
+						} else {
+                             
+                            
+                            
+						}
 
 
 
@@ -61,7 +82,7 @@ $mail->Subject = 'Form I-3 of Student '.$student;
 
 
 
-$mail->Body    = '<h3>Approved Form i-3 </h3> of '.$student.'<br><br>Internship Start Date-:'.$start.'<br>Internship Ends on :'.$end.'<br>Supervisor name -:'.$supname.'<br>Company :'.$company;
+$mail->Body    = '<h3>Approved Form i-3 </h3> of '.$student.'<br><br>Internship Start Date-:'.$start.'<br>Internship Ends on :'.$end.'<br>Supervisor name -:'.$supname.'<br>Company :'.$company.'<br>Problems-:'.$problem.'<br>Summary-:'.$summary;
 $mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
 
 if(!$mail->send()) {
